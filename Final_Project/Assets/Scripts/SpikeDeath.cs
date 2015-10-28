4de042842b121899c6fda4cd32ce578d4f9dcef3
Cosette24601 <crazycosette@gmail.com>
2015-10-28 17:55:50 +0100

    spikey

---
+++ Final_Project/Assets/Scripts/SpikeDeath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerCollider (Collider other){
+		if(other.gameObject.CompareTag("Death")){
+			Application.LoadLevel(Application.loadedLevel); 
+		}
+	}
+}
